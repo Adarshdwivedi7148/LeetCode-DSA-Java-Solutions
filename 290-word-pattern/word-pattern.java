cb8b379489af73a8b7f4ef34dc7a93ac8a5bc5d6
@ads_7148 <136921077+Adarshdwivedi7148@users.noreply.github.com>
2026-09-10 01:14:22 +0530

    Time: 1 ms (83.75%) | Memory: 42.8 MB (55.44%) - LeetSync

---
+++ 290-word-pattern/word-pattern.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        String[] words = s.split(" ");
+
+        if(pattern.length() != words.length) {
+            return false;
+        }
+
+        HashMap<Character, String> map = new HashMap<>();
+        HashMap<String, Character> use = new HashMap<>();
+
+        for(int i=0; i<pattern.length(); i++) {
+            
+            char ch = pattern.charAt(i);
+            String word = words[i];
+
+            if(map.containsKey(ch) && map.get(ch).equals(word) == false) {
+               return false;
+            }
+
+            if(use.containsKey(word) && use.get(word) != ch) {
+               return false;
+            }
+
+            map.put(ch,word);
+            use.put(word,ch);
+        }
+        return true;
+    }
+}
